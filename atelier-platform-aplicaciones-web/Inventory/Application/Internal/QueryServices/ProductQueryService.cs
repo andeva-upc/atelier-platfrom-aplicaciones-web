bf507fc26100ebee9d0d2716daf1c0d5d63c2810
Adiel Sanchez <adiel.sanchez3110@gmail.com>
2026-06-02 11:21:00 -0500

    feat(inventory): add GetProductByIdQuery and QueryHandler

---
+++ atelier-platform-aplicaciones-web/Inventory/Application/Internal/QueryServices/ProductQueryService.cs
@@ -0,0 +1,23 @@
+using System.Threading;
+using System.Threading.Tasks;
+using atelier_platform_aplicaciones_web.Inventory.Application.QueryServices;
+using atelier_platform_aplicaciones_web.Inventory.Domain.Model.Aggregates;
+using atelier_platform_aplicaciones_web.Inventory.Domain.Model.Queries;
+using atelier_platform_aplicaciones_web.Inventory.Domain.Repositories;
+
+namespace atelier_platform_aplicaciones_web.Inventory.Application.Internal.QueryServices;
+
+public class ProductQueryService : IProductQueryService
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductQueryService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<Product?> Handle(GetProductByIdQuery query, CancellationToken cancellationToken = default)
+    {
+        return await _productRepository.FindByIdAsync(query.ProductId, cancellationToken);
+    }
+}
