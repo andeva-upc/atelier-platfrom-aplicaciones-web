fb12faab09413081196f845cdad4b3f0375385dd
danieltyuyu <di0014617@gmail.com>
2026-06-07 18:49:36 -0500

    feat(iot): add domain model value objects.

---
+++ atelier-platform-aplicaciones-web/IoT/Domain/Model/ValueObjects/DtcSeverity.cs
@@ -0,0 +1,9 @@
+namespace atelier_platform_aplicaciones_web.IoT.Domain.Model.ValueObjects;
+
+public enum DtcSeverity
+{
+    LOW,
+    MEDIUM,
+    HIGH,
+    CRITICAL
+}
---
+++ atelier-platform-aplicaciones-web/IoT/Domain/Model/ValueObjects/OBD2DeviceStatus.cs
@@ -0,0 +1,8 @@
+namespace atelier_platform_aplicaciones_web.IoT.Domain.Model.ValueObjects;
+
+public enum OBD2DeviceStatus
+{
+    AVAILABLE,
+    LINKED,
+    NOT_AVAILABLE
+}
