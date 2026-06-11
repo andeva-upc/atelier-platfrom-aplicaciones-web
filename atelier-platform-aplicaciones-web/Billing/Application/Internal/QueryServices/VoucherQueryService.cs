ff4a121e99aacb8c9cb4b11acb458dc043cccfa2
AldoDev20 <u202419485@upc.edu.pe>
2026-06-11 08:02:46 -0500

    feat(billing): implement voucher query service and queries.

---
+++ atelier-platform-aplicaciones-web/Billing/Application/Internal/QueryServices/VoucherQueryService.cs
@@ -0,0 +1,23 @@
+using System.Threading;
+using System.Threading.Tasks;
+using atelier_platform_aplicaciones_web.Billing.Application.QueryServices;
+using atelier_platform_aplicaciones_web.Billing.Domain.Model.Aggregates;
+using atelier_platform_aplicaciones_web.Billing.Domain.Model.Queries;
+using atelier_platform_aplicaciones_web.Billing.Domain.Repositories;
+
+namespace atelier_platform_aplicaciones_web.Billing.Application.Internal.QueryServices;
+
+public class VoucherQueryService : IVoucherQueryService
+{
+    private readonly IVoucherRepository _voucherRepository;
+
+    public VoucherQueryService(IVoucherRepository voucherRepository)
+    {
+        _voucherRepository = voucherRepository;
+    }
+
+    public async Task<Voucher?> Handle(GetVoucherByIdQuery query, CancellationToken cancellationToken = default)
+    {
+        return await _voucherRepository.FindByIdAsync(query.Id);
+    }
+}
