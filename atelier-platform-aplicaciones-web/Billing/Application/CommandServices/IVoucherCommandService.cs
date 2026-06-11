716559adfc9ff26a1847128c6246aa1704bcdecb
AldoDev20 <u202419485@upc.edu.pe>
2026-06-11 10:16:50 -0500

    feat(billing): implement remove payment command service.

--- atelier-platform-aplicaciones-web/Billing/Application/CommandServices/IVoucherCommandService.cs
+++ atelier-platform-aplicaciones-web/Billing/Application/CommandServices/IVoucherCommandService.cs
@@ -10,4 +10,5 @@ public interface IVoucherCommandService
 {
     Task<Result<Voucher>> Handle(GenerateVoucherCommand command, CancellationToken cancellationToken = default);
     Task<Result<atelier_platform_aplicaciones_web.Billing.Domain.Model.Entities.Payment>> Handle(AddPaymentCommand command, CancellationToken cancellationToken = default);
+    Task<Result> Handle(RemovePaymentCommand command, CancellationToken cancellationToken = default);
 }
