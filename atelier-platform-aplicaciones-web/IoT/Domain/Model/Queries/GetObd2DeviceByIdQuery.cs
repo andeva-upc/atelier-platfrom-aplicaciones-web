ca541d1a10826fa6af86ea4c302ef273cf19d2dd
danieltyuyu <di0014617@gmail.com>
2026-06-13 10:02:50 -0500

    feat(iot): define get obd2 device by id query model.

---
+++ atelier-platform-aplicaciones-web/IoT/Domain/Model/Queries/GetObd2DeviceByIdQuery.cs
@@ -0,0 +1,5 @@
+using atelier_platform_aplicaciones_web.IoT.Domain.Model.ValueObjects;
+
+namespace atelier_platform_aplicaciones_web.IoT.Domain.Model.Queries;
+
+public record GetObd2DeviceByIdQuery(Obd2DeviceId Id);
