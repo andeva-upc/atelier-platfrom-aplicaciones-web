e6ab6f3c19846b156901ae9a1ebd947f32404059
danieltyuyu <di0014617@gmail.com>
2026-06-13 10:03:46 -0500

    feat(iot): implement get obd2 device query handling logic.

---
+++ atelier-platform-aplicaciones-web/IoT/Application/Internal/QueryServices/Obd2DeviceQueryService.cs
@@ -0,0 +1,16 @@
+using System.Threading;
+using System.Threading.Tasks;
+using atelier_platform_aplicaciones_web.IoT.Application.QueryServices;
+using atelier_platform_aplicaciones_web.IoT.Domain.Model.Aggregates;
+using atelier_platform_aplicaciones_web.IoT.Domain.Model.Queries;
+using atelier_platform_aplicaciones_web.IoT.Domain.Repositories;
+
+namespace atelier_platform_aplicaciones_web.IoT.Application.Internal.QueryServices;
+
+public class Obd2DeviceQueryService(IObd2DeviceRepository obd2DeviceRepository) : IObd2DeviceQueryService
+{
+    public async Task<Obd2Device?> Handle(GetObd2DeviceByIdQuery query, CancellationToken cancellationToken = default)
+    {
+        return await obd2DeviceRepository.FindObd2DeviceByIdAsync(query.Id, cancellationToken);
+    }
+}
---
+++ atelier-platform-aplicaciones-web/IoT/Application/QueryServices/IObd2DeviceQueryService.cs
@@ -0,0 +1,11 @@
+using System.Threading;
+using System.Threading.Tasks;
+using atelier_platform_aplicaciones_web.IoT.Domain.Model.Aggregates;
+using atelier_platform_aplicaciones_web.IoT.Domain.Model.Queries;
+
+namespace atelier_platform_aplicaciones_web.IoT.Application.QueryServices;
+
+public interface IObd2DeviceQueryService
+{
+    Task<Obd2Device?> Handle(GetObd2DeviceByIdQuery query, CancellationToken cancellationToken = default);
+}
